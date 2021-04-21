5826000c92e9334910bc749a61b5e60ff8415f25
Qeolvdxu <hve0002@uah.edu>
2021-04-20 23:15:41 -0500

    Trimmed the program down

--- src/Board.cpp
+++ src/Board.cpp
@@ -52,46 +52,6 @@ Board::~Board()
 	size = 0;
 }
 
-void Board::printBoard()
-{
-	cout << "\u001b[2J" //clears screen on both windows and unix-likes
-	//<< "\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n"
-    << "The turn belngs to player " << player << "\n\n"
-    << "   ";
-	
-	// print first row
-	for (int i = 0; i < size; i++)
-	{
-		cout << i << ' ';
-		if (i < 10)
-			cout << ' ';
-	}
-	cout << "\n\n";
-
-	// print remaining rows
-    for(int i = 0; i < size; i++)
-    {
-         cout << i << ' ';
-		 if (i < 10)
-			cout << ' ';
-         for (int j = 0; j < size; j++)
-         {
-	         cout << array[i][j]->getChar();
-             if (j < size-1)
-   	         	cout << "--";
-             else
-                cout << '\n'; 
-          }
-          if (i < size-1)
-		  {
-    		cout << "   ";
-			for (int i = 0; i < size; i++)
-				cout << '|' << "  ";
-			cout << "\n";
-		  }
-	}
-}
-
 bool Board::placeStone(int row, int column) //simply gets user input and changes the respective stones character
 {
 	bool valid = false;
