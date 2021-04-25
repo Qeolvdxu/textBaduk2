cff29ae19cc95c4cf0b04e5c732fb7a5807b32db
qeolvdxu <hve0002@uah.edu>
2021-04-25 13:45:28 -0500

    updated scripts

--- install2local.sh
+++ install2local.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+sudo cp include/textbaduk2.h /usr/local/include/textbaduk2.h
 g++ -fPIC -c src/Stone.cpp src/Board.cpp
 sudo gcc -shared *.o -o /usr/local/lib/libtextbaduk2.so
 sudo chmod 755 /usr/local/lib/libtextbaduk2.so
-sudo cp include/textbaduk2.h /usr/local/include/textbaduk2.h
 rm *.o
 echo Done
